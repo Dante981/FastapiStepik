cf74967b70a98ebd39de84ce6a2d2a00529891e7
Victor <novovitya@yandex.ru>
2026-03-21 14:12:52 +0300

    cp3/task1

---
+++ chapter3/task1.py
@@ -0,0 +1,24 @@
+'''
+Создайте приложение с эндпоинтом для чтения всех заметок. Заметки должны хранится в словаре notes_db, где ключ — id (целое число), значение — content (строка).
+
+Эндпоинт:
+
+GET /notes — возвращает словарь всех заметок. Если словарь пуст, возвращает {}.
+
+ Пример: GET /notes → {0: "Study FastAPI", 1: "I like FastAPI"}. 
+ 
+
+Используйте все необходимые импорты из стандартных библиотек Python и FastAPI. 
+
+'''
+
+
+from fastapi import FastAPI, status
+
+app = FastAPI()
+
+notes_db = {0: "Study FastAPI", 1: "I like FastAPI"}
+
+@app.get("/notes", status_code=status.HTTP_200_OK)
+async def read_notes() -> dict:
+    return notes_db
